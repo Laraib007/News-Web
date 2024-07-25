71981df0a474839a1edd2fe22dd646edbee648af
Laraib007 <lrbkhan01@gmail.com>
2024-07-25 22:19:19 +0500

    setting url

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {dec, title, imgUrl} = this.props
+    let {dec, title, imgUrl, url} = this.props
     return (
       <div className='container'>
         <div className="card" style={{width: "18rem"}}>
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{dec}</p>
-    <a href="/" className="btn btn-primary sm-3">Read More</a>
+    <a href={url} className="btn btn-primary sm-3">Read More</a>
   </div>
 </div>
       </div>
