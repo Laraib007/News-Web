b990b03db108d03f209f720cd398775737fbd868
Laraib007 <lrbkhan01@gmail.com>
2024-07-30 19:10:12 +0500

    changing country news through props

--- src/component/NewsComponent.js
+++ src/component/NewsComponent.js
@@ -28,7 +28,7 @@ static propTypes ={
   }
     async componentDidMount() {
       this.setState({loading: true})
-      let data = await fetch(`https://newsapi.org/v2/top-headlines?q=in&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=1&pageSize=${this.props.pageSize}`)
+      let data = await fetch(`https://newsapi.org/v2/top-headlines?${this.state.country}&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=1&pageSize=${this.props.pageSize}`)
       
       let parsedData = await data.json()
       this.setState({
@@ -41,7 +41,7 @@ static propTypes ={
     clickNextHandler= async()=>{
       if(!( Math.ceil(this.state.page + 1>this.state.totalResults/20))){
         this.setState({loading: true})
-      let data = await fetch(`https://newsapi.org/v2/top-headlines?q=in&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`)
+      let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.state.country}&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`)
       
       let parsedData = await data.json()
   this.setState({
@@ -54,7 +54,7 @@ static propTypes ={
 
  clickPrevHandler= async()=>{
   this.setState({loading: true})
-  let data = await fetch(`https://newsapi.org/v2/top-headlines?q=in&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`)
+  let data = await fetch(`https://newsapi.org/v2/top-headlines?${this.state.country}&apiKey=e98033f9dc4145e7a23f605a8fe60d1c&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`)
  
   let parsedData = await data.json()
 this.setState({
